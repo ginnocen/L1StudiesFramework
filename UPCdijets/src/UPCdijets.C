98828ccb2fb67f6ee32bc62151a05fe85d60504c
Gian Michele Innocenti <gminnocen@gmail.com>
2023-07-29 15:32:01 +0200

    Update input files + temporary removal of ZDC selection (#2)

--- UPCdijets/src/UPCdijets.C
+++ UPCdijets/src/UPCdijets.C
@@ -64,19 +64,22 @@ void FillChain(TChain& chain, vector<string>& files) {
     }
 }
 
-int UPCdijets(char const* input="/eos/user/g/ginnocen/CMSRun3/ZeroBias_HIRun2018A_MiniAOD_Run326776_131X_HF_14_19_v1/230701_025938/0000/",
-	      bool doZDCOr=true, float ZDC_1nthreshold=2000) {
+int UPCdijets(char const* input= "/afs/cern.ch/work/g/ginnocen/public/CMSwork/myUPCMCsamplesRun3/20230729_UPCdijet/20230729_UPCdijet_CMSSW_13_1_0_pre4_L1Forest/",
+//int UPCdijets(char const* input= "/eos/user/g/ginnocen/CMSRun3/ZeroBias_HIRun2018A_MiniAOD_Run326776_131X_HF_14_19_v1/230701_025938/0000/",
+//int UPCdijets(char const* input= "data/",
+	      bool doZDCOr=false, float ZDC_1nthreshold=2000) {
     /* read in all files in the input folder */
     vector<string> files;
     GetFiles(input, files);
- 
+
     /* read in offline ZDC information */
+   /*
     TChain offChainZDC("zdcanalyzer/zdcrechit");
     FillChain(offChainZDC, files);
     TTreeReader offReaderZDC(&offChainZDC);
     TTreeReaderValue<float> sumPlus(offReaderZDC, "sumPlus");
     TTreeReaderValue<float> sumMinus(offReaderZDC, "sumMinus");
-
+    */
     /* read in calo jet information */
     TChain offChain("akCs4PFJetAnalyzer/t");
     FillChain(offChain, files);
@@ -98,29 +101,28 @@ int UPCdijets(char const* input="/eos/user/g/ginnocen/CMSRun3/ZeroBias_HIRun2018
     float threshold = 8;
 
     /* create histograms for efficiency plots */
-    int nbins = 20;
+    int nbins = 40;
     float min = 0;
     float max = 40;
-
     TH1F emuHist("emuHist", "", nbins, min, max);
     TH1F emuMatchedHist("emuMatchedHist", "", nbins, min, max);
     TH1F recoHist("recoHist", "", nbins, min, max);
 
     Long64_t totalEvents = emuReader.GetEntries(true);
-
     int counter_ZDCOrselected = 0;
     int counter_ZDCOrJetselected = 0;
     /* read in information from TTrees */
+    int totnjets = 0;
     for (Long64_t i = 0; i < totalEvents; i++) {
-        emuReader.Next(); offReader.Next(); offReaderZDC.Next();
-        if (i % 500000 == 0) {
+        emuReader.Next(); offReader.Next(); //offReaderZDC.Next();
+        if (i % 100 == 0) {
             cout << "Entry: " << i << " / " <<  totalEvents << endl;
         }
 	//FIXME: this is a very preliminary attempt to apply an offline-like selection on the ZDC
 	//signal. Later on we will use it to mimic the effect of a ZDCOr L1 selection
-	bool isofflinesignal=((*sumPlus)>ZDC_1nthreshold&&(*sumMinus)<ZDC_1nthreshold)||((*sumPlus)<ZDC_1nthreshold&&(*sumMinus)>ZDC_1nthreshold);
-        if (doZDCOr && !isofflinesignal) continue;
-        counter_ZDCOrselected++;
+	//bool isofflinesignal=((*sumPlus)>ZDC_1nthreshold&&(*sumMinus)<ZDC_1nthreshold)||((*sumPlus)<ZDC_1nthreshold&&(*sumMinus)>ZDC_1nthreshold);
+        //if (doZDCOr && !isofflinesignal) continue;
+        //counter_ZDCOrselected++;
 
 	float maxJetPt = -999;
         float maxJetPhi = -999;
@@ -129,10 +131,11 @@ int UPCdijets(char const* input="/eos/user/g/ginnocen/CMSRun3/ZeroBias_HIRun2018
         float emuMaxJetPt = -999;
         float emuMatchedJetPt = -999;
         float minDR = 10;
-        //std::cout<<"N jets="<< *jetN<<std::endl;
         /* iterate through jets and find the jet with max pT */
-        for (int i = 0; i < *jetN; ++i) {
-            if (TMath::Abs(jetEta[i]) > 2) { continue; }
+	//std::cout<<"N jets= "<<*jetN<<std::endl;
+	totnjets = totnjets + *jetN;
+	for (int i = 0; i < *jetN; ++i) {
+            if (TMath::Abs(jetEta[i]) > 9) { continue; }
 
             if (jetPt[i] > maxJetPt) {
                 maxJetPt = jetPt[i];
@@ -208,8 +211,9 @@ int UPCdijets(char const* input="/eos/user/g/ginnocen/CMSRun3/ZeroBias_HIRun2018
     recoHist.Write();
 
     fout->Close();
+    std::cout<<"N total jets= "<<totnjets<<std::endl;
     std::cout<<"ZDCOrselected fraction ="<<(float)counter_ZDCOrselected/(float)totalEvents<<std::endl;
-    std::cout<<"ZDCOrJetselected, threshold="<<threshold<<" is ="<<(float)counter_ZDCOrJetselected/(float)totalEvents<<std::endl; 
+    std::cout<<"ZDCOrJetselected, threshold="<<threshold<<" is ="<<(float)counter_ZDCOrJetselected/(float)totalEvents<<std::endl;
     return 1;
 }
 
